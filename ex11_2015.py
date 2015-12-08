602fa875ebb30bf43134e38652a9d16a40683ce4
Rachel Decker <rachelpieperdecker@gmail.com>
2015-12-07 22:14:21 -0800

    Create ex11_2015.py

---
+++ ex11_2015.py
@@ -0,0 +1,14 @@
+"""
+Rachel Pieper
+Learn Python the Hard Way
+#11
+10/9/2015
+"""
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r years old, %r tall and weigh %r pounds." % (age, height, weight)
