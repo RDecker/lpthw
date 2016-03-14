eb0e16fb4fd04d79db6bc80e2ff278a9dbecb4b5
Rachel Decker <rachelpieperdecker@gmail.com>
2016-03-14 15:23:51 -0700

    Added files via upload

---
+++ ex022.py
@@ -0,0 +1,9 @@
+"""
+Rachel Pieper Decker
+Learn Python the Hard Way
+#22
+3/14/2016
+"""
+No code, all review.
+1. Go back through every exercise you have done so far and write down every word and symbol (another name for "character") that you have used. Make sure your list of symbols is complete.
+2. Next to each word or symbol, write its name and what it does. If you can't find a name for a symbol in this book, then look for it online. If you do not know what a word or symbol does, then read about it again and try using it in some code.
